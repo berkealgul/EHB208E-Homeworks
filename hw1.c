0e274804cd7da07df2c5b63fc6ed2527b5fc4d16
berkealgul <berkealgul@gmail.com>
2023-11-07 19:00:37 +0300

    initial commit

---
+++ hw1.c
@@ -0,0 +1,43 @@
+// Berke Algül
+// 040230762
+// 7.11.2023
+
+#include "stdio.h"
+#include "string.h"
+
+
+
+typedef struct Matrix_Node {
+    Matrix_Node* pNext_column;
+    Matrix_Node* pNext_row;
+    int nData;
+} Matrix_Node;
+
+typedef struct Matrix {
+    Matrix_Node *pNode;
+} Matrix;
+
+Matrix* matrix_from_csv(const char*);
+Matrix* print_matrix_to_txt(const char *);
+
+
+int main()
+{
+    Matrix* matrix = matrix_from_csv("input.csv"); 
+
+    if(!matrix) { return 1; } // terminate the program if matrix is null pointer
+
+
+    return 0;
+}
+
+Matrix* matrix_from_csv(const char *pszInputFilePath)
+{
+    FILE *pFile = fopen(pszInputFilePath, "r");
+
+    if(!pFile)
+    {
+        printf("Couldn't read input file. Terminating!");
+        return NULL;
+    }
+}
