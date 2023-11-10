7c2b20184b28303c7d8d89dbbcf3806344faa09d
berkealgul <berkealgul@gmail.com>
2023-11-10 21:55:04 +0300

    initialşiztion implemented

--- hw1.c
+++ hw1.c
@@ -14,12 +14,12 @@ typedef struct Matrix_Node {
 } Matrix_Node;
 
 typedef struct Matrix {
-    Matrix_Node *pNode;
+    Matrix_Node *pHead_node;
 } Matrix;
 
 // main functions
+void print_matrix_to_txt(const char*, const Matrix*);
 Matrix* matrix_from_csv(const char*);
-Matrix* print_matrix_to_txt(const char *);
 
 //helper functions
 Matrix* insert_data_to_matrix(Matrix*, int, bool);
@@ -29,11 +29,26 @@ int main()
 {
     Matrix* matrix = matrix_from_csv("input.csv"); 
 
-    //if(!matrix) { return 1; } // terminate the program if matrix is null pointer
+    if(!matrix) { return 1; } // terminate the program if matrix is null pointer
 
-
+    print_matrix_to_txt("output_print.txt", matrix);
 
     return 0;
+}
+
+void print_matrix_to_txt(const char* pszOutputFilePath, const Matrix* pM)
+{
+    Matrix_Node* pHead = pM->pHead_node;
+
+    while(pHead)
+    {
+        printf("%d ", pHead->nData);
+
+        if(pHead->pNext_column)
+        {
+        
+        }
+    }
 }
 
 Matrix* matrix_from_csv(const char *pszInputFilePath)
@@ -49,54 +64,83 @@ Matrix* matrix_from_csv(const char *pszInputFilePath)
     char row[100000]; //row with predefined maximum char size for row of csv file
     char *cToken;
     
+    Matrix* pM = malloc(sizeof(Matrix));
+
     // read each row 
     while (!feof(pFile))
     {
         fgets(row, 100000, pFile);
-
         cToken = strtok(row, ",");
+        bool bNewRow = true;
 
-        // extract tokens from colums
+        // extract data from colums
         while(cToken != NULL)
         {
             int nData = strtol(cToken, NULL, 10);
-
-
-
+            insert_data_to_matrix(pM, nData, bNewRow);
             cToken = strtok(NULL, ",");
+            bNewRow = false; 
         }
     }
+
+    return pM;
 }
 
+
+/* 
+    Data insertion starts from top left and values are inserted in row major order
+
+    1, 2, 3            
+    4, 5, 6
+    7, 8, 9
+*/
 Matrix* insert_data_to_matrix(Matrix* pM, int nData, bool bNewRow)
 {
-    // 
-    if(!pM->pNode)
+    Matrix_Node* pNode = generate_matrix_node(nData);
+
+    if(!pM->pHead_node) // If head node is empty, then generate it and return
     {
-        pM->pNode = generate_matrix_node(nData);
+        pM->pHead_node = pNode;
         return pM;
     }
 
-    if(bNewRow)
+    //Iterate to last row
+    Matrix_Node* pHead = pM->pHead_node;
+    while(pHead->pNext_row)
     {
-
+        pHead = pHead->pNext_row;
     }
-    
+
+    // If we create new row, then insert new row node
+    if(bNewRow) 
+    {
+        pHead->pNext_row = pNode;
+    }
+    // else insert data to end of last row
+    else
+    {
+        // Iterate head node to last column and insert new column node 
+        while(pHead->pNext_column)
+        {
+            pHead = pHead->pNext_column;
+        }
+        pHead->pNext_column = pNode;
+    }
 
     return pM;
 }
 
 Matrix_Node* generate_matrix_node(int nData)
 {
-    Matrix_Node *pNode = malloc(sizeof(Matrix_Node));
+    Matrix_Node *pHead_node = malloc(sizeof(Matrix_Node));
 
-    if(!pNode)
+    if(!pHead_node)
     {
         printf("ERROR: Memory allocation during node generation has failed!!!");
         return NULL;
     }
 
-    pNode->nData = nData;
+    pHead_node->nData = nData;
 
-    return pNode;
+    return pHead_node;
 }
