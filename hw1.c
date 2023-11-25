06a0043cdf5a341d9e3c8d6ea5bf848a1d710dc8
berkealgul <berkealgul@gmail.com>
2023-11-25 03:48:47 +0300

    tasks fixxed and matrix creation & priting done

--- hw1.c
+++ hw1.c
@@ -18,8 +18,9 @@ typedef struct Matrix {
 } Matrix;
 
 // main functions
-void print_matrix_to_txt(const char*, const Matrix*);
 Matrix* matrix_from_csv(const char*);
+void print_matrix_to_txt(const char*, const Matrix*);
+void print_diagonals_to_txt(const char*, const Matrix*);
 
 //helper functions
 Matrix* insert_data_to_matrix(Matrix*, int, bool);
@@ -29,26 +30,40 @@ int main()
 {
     Matrix* matrix = matrix_from_csv("input.csv"); 
 
-    if(!matrix) { return 1; } // terminate the program if matrix is null pointer
-
     print_matrix_to_txt("output_print.txt", matrix);
 
     return 0;
 }
 
+void print_diagonals_to_txt(const char* pszOutputFilePath, const Matrix* pM)
+{
+
+}
+
 void print_matrix_to_txt(const char* pszOutputFilePath, const Matrix* pM)
 {
+    FILE *pFile = fopen(pszOutputFilePath, "w");
     Matrix_Node* pHead = pM->pHead_node;
+    Matrix_Node* pRow_head = pHead;
 
-    while(pHead)
+    while(pHead != NULL) // empty matrix will be discarded
     {
-        printf("%d ", pHead->nData);
-
-        if(pHead->pNext_column)
+        fprintf(pFile, "%d", pHead->nData); // print data
+        if(pHead->pNext_column != NULL) // traverse row
         {
-        
+            pHead = pHead->pNext_column;
+            fprintf(pFile, ","); // print coma if we traverse the row
         }
+        else if(pRow_head->pNext_row != NULL) // if row is done, go to next row
+        {
+            pRow_head = pRow_head->pNext_row;
+            pHead = pRow_head;
+            fprintf(pFile, "\n"); // print next line for next row
+        }
+        else { break; } // break if we 
     }
+
+    fclose(pFile);
 }
 
 Matrix* matrix_from_csv(const char *pszInputFilePath)
@@ -63,8 +78,8 @@ Matrix* matrix_from_csv(const char *pszInputFilePath)
 
     char row[100000]; //row with predefined maximum char size for row of csv file
     char *cToken;
-    
     Matrix* pM = malloc(sizeof(Matrix));
+    pM->pHead_node = NULL;
 
     // read each row 
     while (!feof(pFile))
@@ -83,6 +98,7 @@ Matrix* matrix_from_csv(const char *pszInputFilePath)
         }
     }
 
+    fclose(pFile);
     return pM;
 }
 
@@ -98,7 +114,7 @@ Matrix* insert_data_to_matrix(Matrix* pM, int nData, bool bNewRow)
 {
     Matrix_Node* pNode = generate_matrix_node(nData);
 
-    if(!pM->pHead_node) // If head node is empty, then generate it and return
+    if(pM->pHead_node == NULL) // If head node is empty, then generate it and return
     {
         pM->pHead_node = pNode;
         return pM;
@@ -106,8 +122,10 @@ Matrix* insert_data_to_matrix(Matrix* pM, int nData, bool bNewRow)
 
     //Iterate to last row
     Matrix_Node* pHead = pM->pHead_node;
-    while(pHead->pNext_row)
+    Matrix_Node* pPrev_head;
+    while(pHead->pNext_row != NULL)
     {
+        pPrev_head = pHead;
         pHead = pHead->pNext_row;
     }
 
@@ -119,12 +137,23 @@ Matrix* insert_data_to_matrix(Matrix* pM, int nData, bool bNewRow)
     // else insert data to end of last row
     else
     {
-        // Iterate head node to last column and insert new column node 
-        while(pHead->pNext_column)
+        // Iterate head and prev_head node to last column and insert new column node 
+        while(pHead->pNext_column != NULL)
         {
             pHead = pHead->pNext_column;
+
+            if(pPrev_head != NULL) 
+            {
+                pPrev_head = pPrev_head->pNext_column;
+            }
         }
+
         pHead->pNext_column = pNode;
+
+        if(pPrev_head != NULL) 
+        {
+            pPrev_head->pNext_column->pNext_row = pNode; 
+        }
     }
 
     return pM;
@@ -134,13 +163,15 @@ Matrix_Node* generate_matrix_node(int nData)
 {
     Matrix_Node *pHead_node = malloc(sizeof(Matrix_Node));
 
-    if(!pHead_node)
+    if(pHead_node == NULL)
     {
         printf("ERROR: Memory allocation during node generation has failed!!!");
-        return NULL;
+        exit(1);
     }
 
     pHead_node->nData = nData;
+    pHead_node->pNext_row = NULL;
+    pHead_node->pNext_column = NULL;
 
     return pHead_node;
 }
